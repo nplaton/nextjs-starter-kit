216f40e6d94945df8c410b1a3b36c1ecd70e696c
Nacho Platon <iplaton@mytraffic.fr>
2024-10-20 14:48:44 +0200

    Removed unneeded files

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,6 @@ export default function middleware(req: any) {
     return NextResponse.next();
   }
 }
-
 export const middlewareConfig = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
